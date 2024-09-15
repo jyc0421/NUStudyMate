03a4c3f19e9cff1dcabc21ff97a500b929228692
jyc <jyc0421@foxmail.com>
2024-09-15 23:40:39 +0800

    jyc commit

---
+++ src/main/java/com/jyc/nustudymate/NuStudyMateApplication.java
@@ -0,0 +1,13 @@
+package com.jyc.nustudymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuStudyMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuStudyMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyc/nustudymate/NuStudyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyc.nustudymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuStudyMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
