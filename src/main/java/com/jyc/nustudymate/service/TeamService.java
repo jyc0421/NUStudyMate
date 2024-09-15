a5fe849dddb86d72796f9adef331a422f0b74121
jyc <jyc0421@foxmail.com>
2024-09-16 04:25:26 +0800

    jyc commit

---
+++ src/main/java/com/jyc/nustudymate/service/TeamService.java
@@ -0,0 +1,12 @@
+package com.jyc.nustudymate.service;
+
+import com.jyc.nustudymate.model.domain.Team;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author jyc
+* @description 针对表【team(队伍)】的数据库操作Service
+*/
+public interface TeamService extends IService<Team> {
+
+}
