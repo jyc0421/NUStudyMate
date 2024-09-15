c0745f1f83e57fc6c8d0d6d5f9d8ed0aa225c077
jyc <jyc0421@foxmail.com>
2024-09-16 04:29:25 +0800

    jyc commit

--- src/main/java/com/jyc/nustudymate/mapper/UserTeamMapper.java
+++ src/main/java/com/jyc/nustudymate/mapper/UserTeamMapper.java
@@ -6,7 +6,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 /**
 * @author jyc
 * @description 针对表【user_team(用户队伍关系)】的数据库操作Mapper
-* @createDate 2024-09-16 03:46:21
 * @Entity com.jyc.nustudymate.model.domain.UserTeam
 */
 public interface UserTeamMapper extends BaseMapper<UserTeam> {
